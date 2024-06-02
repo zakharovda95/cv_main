98e02b80e78d8c27f58e915189ffae0303dd6051
zakharovda95 <zakharovda95@gmail.com>
2024-06-02 15:03:17 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
+  modules: [
+    '@nuxt/eslint'
+  ],
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true
+    }
+  }
+})
